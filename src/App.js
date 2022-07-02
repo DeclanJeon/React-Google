d159f2e2c604d12ca9d522c6ad9414f4b8a72eb5
declan <syas0301@gmail.com>
2022-07-02 21:13:06 +0900

    first commit/ term을 못가져옴

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Home from "./pages/Home";
+import SearchPage from "./pages/SearchPage";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className='App'>
+            <Router>
+                {/* Home (the one with the search on) */}
+                <Routes>
+            <Route path='/search' element={ SearchPage} />
+                    <Route path='/' element={<Home />} />
+                </Routes>
+
+                {/* SearchPage (The Results apge) */}
+            </Router>
+        </div>
+    );
 }
 
 export default App;
